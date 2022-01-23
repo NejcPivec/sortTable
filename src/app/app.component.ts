c6130802945db13582fd055793a60b0705fc0ed8
NejcPivec <nejc.pivec1994@gmail.com>
2022-01-23 19:15:36 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, Validators} from "@angular/forms";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sorting-table';
+  form = this.fb.group({
+      lessons: this.fb.array([])
+  });
+
+  public simpleList = [
+    {'name': 'John'},
+    {'name': 'Smith'},
+    {'name': 'George'},
+    {'name': 'Luka'},
+    {'name': 'Nejc'},
+    {'name': 'Maj'},
+    {'name': 'Nace'},
+  ];
+
+  activeNote: string = '';
+  constructor(private fb:FormBuilder) {}
+
+  get lessons() {
+    return this.form.controls["lessons"] as FormArray;
+  }
+
+  addLesson() {
+    const lessonForm = this.fb.group({
+      input1: ['', Validators.required],
+      input2: ['', Validators.required],
+      input3: ['', Validators.required],
+    });
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
+  }
+
+  public removeItem(item: any, list: any[]): void {
+    list.splice(list.indexOf(item), 1);
+  }
+
+  onDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.form.get('lessons')?.value, event.previousIndex, event.currentIndex);
+  }
+
+  moveUp(index: number) {
+    // console.log(this.lessons.controls);
+    this.moveInArray(this.lessons.controls, index, index - 1);
+  }
+
+  moveDown(index: number) {
+    this.moveInArray(this.lessons.controls, index, index + 1);
+  }
+
+  moveInArray(arr: any, from: number, to: number) {
+
+    // Make sure a valid array is provided
+    if (Object.prototype.toString.call(arr) !== '[object Array]') {
+      throw new Error('Please provide a valid array');
+    }
+
+    // Delete the item from it's current position
+    const item = arr.splice(from, 1);
+
+    // Make sure there's an item to move
+    if (!item.length) {
+      throw new Error('There is no item in the array at index ' + from);
+    }
+
+    // Move the item to its new position
+    arr.splice(to, 0, item[0]);
+
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DndListModule} from 'ngx-drag-and-drop-lists';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DndListModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
